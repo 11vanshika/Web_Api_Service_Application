efe21fbe6d3c27c40229aeb7772c1782ed841e63
11vanshika <vanshikaa@moback.com>
2022-11-07 18:22:29 +0530

    Api_Services

---
+++ Api_WebApplication1_Services_Class/Model/Employee_Class.cs
@@ -0,0 +1,10 @@
+﻿namespace Api_WebApplication1_Services_Class.Model
+{
+    public class Employee_Class
+    {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+
+        public string EmailId { get; set; }
+    }
+}
